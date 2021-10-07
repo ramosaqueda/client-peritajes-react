63279fb6f7bd24adcd93c7c83aef141c47fa54b5
ramosaqueda <ramos.aqueda@gmail.com>
2021-10-06 22:53:30 -0300

    ok

--- src/functions/fiscal.js
+++ src/functions/fiscal.js
@@ -2,15 +2,9 @@ import axios from "axios";
 
 export const getFiscales = async () =>
   await axios.get(`${process.env.REACT_APP_API}/fiscales`);
-<<<<<<< HEAD
-/*
+
 export const getFiscal = async (slug) =>
   await axios.get(`${process.env.REACT_APP_API}/Fiscal/${slug}`);
-=======
-
-export const getFiscal = async (_id) =>
-  await axios.get(`${process.env.REACT_APP_API}/readfiscal/${_id}`);
->>>>>>> 136c43501ce6c36bc5c222dbdedcf5fb94e79f5a
 
 export const removeFiscal = async (_id, authtoken) =>
   await axios.delete(`${process.env.REACT_APP_API}/removefiscal/${_id}`, {
